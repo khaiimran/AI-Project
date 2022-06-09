93aa6d9005665dd325294f7a0bf018fd2a7e951c
Khairul Imran <khaiimran72@gmail.com>
2022-06-10 01:21:53 +0800

    Update on main.py and driver.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import driver
 def main():
     template = 'logo_train.png'
     choice = input("Enter 0 to upload image, otherwise it will run using camera: ")
-    if choice != 0:
+    if choice != '0':
         driver.cam(template)
     else:
-        image = 'test.apng'
+        image = input("Enter the full path of the image (only .png): ")
+        if image is None:
+            image = 'test.apng'
         driver.img(template, image)
 
 if __name__ == '__main__':
