14be67d1f5186a1931dfc9cd7f9a5368231043a6
Khairul Imran <khaiimran72@gmail.com>
2022-06-09 13:44:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import driver
 
 def main():
     template = 'logo_train.png'
-    choice = int (input())
+    choice = input("Enter 0 to upload image, otherwise it will run using camera: )
     if choice != 0:
         driver.cam(template)
     else:
